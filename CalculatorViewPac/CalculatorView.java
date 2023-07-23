b3c7cd1042ef215bc5e19eeb14dfdb5b20976bc9
Egor Baranovskiy <egorpifagop@gmail.com>
2023-07-23 04:54:13 +0300

    Homework was done

--- CalculatorViewPac/CalculatorView.java
+++ CalculatorViewPac/CalculatorView.java
@@ -7,7 +7,7 @@ public class CalculatorView{
 
     public void displayResult(int result, CalculatorModel model) {
         String resultString = String.format("%d %s %d = %d", model.getNumber1(), model.getParameter(), model.getNumber2(),result);
-        model.logger(resultString, "C:\\Users\\User\\Desktop\\\u041D\u043E\u0432\u0430\u044F \u043F\u0430\u043F\u043A\u0430\\CalculatorViewPac\\log.txt");
+        model.logger(resultString, "C:\\Users\\User\\Desktop\\\u041D\u043E\u0432\u0430\u044F \u043F\u0430\u043F\u043A\u0430\\log.txt");
         System.out.printf(resultString);
     }
 
